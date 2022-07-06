99a2fc79a7980494a6755fa9fd21375715e69180
Derek Hall <Hderek22@icloud.com>
2022-07-05 23:10:29 -0400

    about me edit

--- pages/about.py
+++ pages/about.py
@@ -3,31 +3,23 @@ import streamlit as st
 
 def write():
     """Used to write the about page in the app.py file"""
-    st.title("My name is Derek Hall")
+    st.title("The name is Hall...Derek Hall")
     st.markdown(
-            """## Who Am I?
-"_Focus on the success of your Teammates and your life will change_"
+            """
+"_Focus on the success of your Teammates and amazing things will happen!_"
 
-One thing you should know about me is...I don't dabble. Its simply not in my nature. If I set out to accomplish a goal, I'm all in, end of story.
+One thing you should know about me is...I don't dabble. It is simply not in my nature. If I set out to accomplish a goal, I'm all in, end of story.
 
 I started bussing tables as a teenager and liked the business...at age 30 I owned 3 restaurants.
 
-I joined retail after selling my business of 14 years...within 3 years I was managing 150M annualy.
+I pursued big box retail after selling my business of 14 years...within 3 years I was managing 150M annualy.
 
-I took up running to get healthy and relieve stress...ended up running the NY marathon twice.
+I took up running to get healthy and ended up running the NYC marathon twice.
 
 Its safe to say I'm certainly not one to dabble.
-
-------
-
-de·ci·sion
-/dəˈsiZHən/
-
-I like to think of a decision being the opposite of an 'incision'. instead of cutting in...you are cutting off. Cutting off any other option and deciding on one outcome.
-
 ## Experiences
 
-**Business Development | Leadership | Team Building**
+**Business Development | Leadership | Team Building | Finance**
  
 ## Skills
 
@@ -37,10 +29,18 @@ I like to think of a decision being the opposite of an 'incision'. instead of cu
 
 ## My Goals
 
+Join a team with likeminded 'can do' teammates and build a 4 star business!
+
 -----
 
-Check out my [**GitHub**](https://github.com/Hderek22/Streamlit_resume.git) for the implementation. Reach out to me for any project or a simple discussion on Streamlit.
-Built Streamlit [**page**](https://www.streamlit.io/) for more more information and updates.
+#### Streamlit is amazing!
+
+__See my [**page**](https://www.streamlit.io/) for more information and updates.__
+
+**Check out my [**GitHub**](https://github.com/Hderek22/Streamlit_resume.git) for the implementation details of this page.** 
+
+**Reach out to me for any project or a simple discussion on Streamlit.**
+
 
 
 """,
