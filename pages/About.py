3fb5417974b56dee9f5a60ea5001523565b2b228
Derek Hall <Hderek22@icloud.com>
2023-05-06 00:53:35 -0400

    edited about page

--- pages/About.py
+++ pages/About.py
@@ -10,15 +10,6 @@ st.markdown(
 
 "_Focus on the success of your Teammates and amazing things will happen for you!_"
 
-One thing you should know about me is...I don't dabble. It is simply not in my nature. If I set out to accomplish a goal, I'm all in, end of story.
-
-I started bussing tables as a teenager and liked the business...at age 30 I owned 3 restaurants.
-
-I pursued big box retail after selling my business of 14 years...within 3 years I was managing 150M annualy.
-
-I took up running to get healthy and ended up running the NYC marathon twice.
-
-Its safe to say I'm certainly not one to dabble.
 ## Experiences
 
 **Business Development | Leadership | Team Building | Finance**
